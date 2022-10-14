0abb7a9af71270ffb2cba64f86625e703b512a9d
rajeeb <rajeeb@bitmascot.com>
2022-10-14 22:33:30 +0600

    optional chain fix

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -7,13 +7,13 @@ const Posts = ({setCurrentId}) => {
   
   const {posts,isLoading} = useSelector((state) => state.posts);
 
-  if(!posts.length && !isLoading) {return "No posts published yet";}
+  if(!posts?.length && !isLoading) {return "No posts published yet";}
 
   return (
     isLoading ? <CircularProgress/> : (
       <Grid sx={{ display: "flex", alignItems: "center",}} container alignItems='stretch' spacing={3}>
             {
-              posts.map((post) =>(
+              posts?.map((post) =>(
                 <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
                     <Post post={post}  setCurrentId={setCurrentId}/>
                 </Grid>
